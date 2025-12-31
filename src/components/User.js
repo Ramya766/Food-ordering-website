223f6154de2bc928713ebc33451df917c844ba75
230701262 <ramyasreevarshini766@gmail.com>
2025-12-31 07:17:00 +0530

    Update the StateVariable in Class

--- src/components/User.js
+++ src/components/User.js
@@ -1,8 +1,11 @@
+import {useState} from "react";
 const User=({name})=>{
+    const[count,Setcount]=useState(0);
+    console.log(count);
     return(
         <div className="user-card">
             <h2>{name}</h2>
-            <h3>FullStack Developer</h3>
+            <button onClick={()=>Setcount(1)}>FullStack Developer</button>
             <h4>Chennai</h4>
         </div>
     )
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -2,16 +2,28 @@ import React from "react";
 class UserClass extends React.Component{
       constructor(props){
             super(props) //It's compulsory to use //if not initialized we can't acess this
-            console.log(props)
+            this.state={
+                count:0,
+              
+            };
         }
+         
+        
         
     render(){
         const  {name}=this.props;
-        
+        const{count}=this.state;
         return(
             
             <div className="user-card">
-             
+             <h1>Count:{count}</h1>
+            <button onClick={()=>{
+                //Never update state variables directly
+                //this.state.count=this.state.count+1 [Wrong];
+                this.setState({
+                    count:this.state.count+1,
+                });
+            }}>Click me</button>
                 <h2>{name}</h2>
                 <h3>Chennai</h3>
                 <h3>Software Developer,Programmer</h3>
