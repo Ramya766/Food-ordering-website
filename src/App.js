e97731fb79b4242c8e64ce619cc94482195af93a
230701262 <ramyasreevarshini766@gmail.com>
2026-01-06 06:52:16 +0530

    Lazy Loading

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React ,{lazy,Suspense} from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
@@ -7,6 +7,9 @@ import About from "./components/About";
 import Error from "./components/Error";
 import Contact from "./components/Contact";
 import RestaurantMenu from "./components/RestaurantMenu";
+//import Grocery from "./components/Grocery";
+
+const Grocery=lazy(()=>import("./components/Grocery"));
 
 
 
@@ -48,6 +51,10 @@ const Router = createBrowserRouter([
       {
         path: "/contact",
         element: <Contact />,
+      },
+       {
+        path: "/grocery",
+        element:<Suspense fallback={<h1>Loading....</h1>}><Grocery /></Suspense> ,
       },
       {
         path:"/restaurants/:resid", //:resid is a dynamic part
---
+++ src/components/Grocery.js
@@ -0,0 +1,6 @@
+const Grocery=()=>{
+    return <div>
+        <h1>This is Grocery Page,you can buy whatever you want!!</h1>
+    </div>
+}
+export default Grocery;
