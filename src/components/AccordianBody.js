dd2b7cb408542c3990aacc9f66fb76137976168e
230701262 <ramyasreevarshini766@gmail.com>
2026-01-26 17:00:29 +0530

    My own accordian

--- src/components/AccordianBody.js
+++ src/components/AccordianBody.js
@@ -1,14 +1,31 @@
+import { CDN_URL } from "../utils/constants";
 const AccordianBody = ({ items }) => {
   console.log(items);
   return (
     <div>
       {items.map((item) => (
-        <div key={item?.card?.info?.id}>
-          <div>
-            <span>{item.card.info.name}</span>
-            <span>{item.card.info.price / 10}</span>
+        <div
+          key={item?.card?.info?.id}
+          className="p-4 m-2 shadow-md border-gray-400 border-b-2 text-left  justify-between flex"
+        >
+          <div className="w-9/12 ">
+            <span className="font-semibold">{item.card.info.name}</span>
+            <span> - ₹{item.card.info.price / 10}</span>
+            <p className=" text-md my-5">{item.card.info.description}</p>
           </div>
-          <p>{item.card.info.description}</p>
+
+          <div className="w-3/12 p-2 ">
+            <div className=" absolute rounded-2xl shadow-2xl">
+              <button className=" bg-white text-black p-2 rounded-lg mx-2 my-2 border border-gray-700">
+                Add +
+              </button>
+            </div>
+            <img
+              className="w-30 "
+              src={CDN_URL + item.card.info.imageId}
+              alt="img"
+            />
+          </div>
         </div>
       ))}
     </div>
--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -1,17 +1,24 @@
 import AccordianBody from "./AccordianBody";
-
+import { useState,} from "react";
 const RestaurantCategory = ({ data }) => {
+  const [showItem, setShowItem] = useState(false);
+  const handleChange = () => {
+    setShowItem(!showItem);
+  };
   return (
     <div>
       {/**Header*/}
-      <div className=" bg-gray-300 w-6/12 mx-auto my-4 shadow-lg p-3 rounded-lg  ">
+      <div className=" bg-gray-200 w-6/12 mx-auto my-4 shadow-lg p-3 rounded-lg  ">
         <div className="flex justify-between">
-          <span className=" font-bold text-lg">
+          <span className=" font-bold text-lg cursor-pointer" onClick={handleChange}>
             {data.title} ({data.itemCards.length})
           </span>
           <span>⬇️</span>
         </div>
-        <AccordianBody items={data?.itemCards} key={data?.itemCards?.card?.info?.id}/>
+        { showItem && <AccordianBody
+          items={data?.itemCards}
+          key={data?.itemCards?.card?.info?.id}
+        />} 
       </div>
       {/**Accordian Body */}
     </div>
